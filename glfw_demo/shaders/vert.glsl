0692ed3a90a7ce6e55000bfeec8050729395a241
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2022-10-23 19:28:22 +0200

    demo project upt

--- glfw_demo/shaders/vert.glsl
+++ glfw_demo/shaders/vert.glsl
@@ -1,21 +1,45 @@
 #version 330 core
 
+#define pi 3.1415
+
 in vec2 aPos;
 
 uniform float aspect = 9.0 / 16.0;
-uniform float zoom = 1.0;
+
 uniform vec2 cam_pos = vec2(0.0);
+uniform float cam_rot = 0;
+uniform float zoom = 1.0;
 
 uniform vec2 entity_pos;
-uniform float entity_rot;
+uniform float entity_rot = 0;
+
+mat3 createMatrix(vec2 pos, float rot, float scale) {
+    float c = cos(rot) * scale;
+    float s = sin(rot) * scale;
+    return mat3(
+        c, s, pos.x,
+        -s, c, pos.y,
+        0, 0, 1
+    );
+}
+
+mat3 createMatrixInv(vec2 pos, float rot, float scale) {
+    float c = cos(rot) / scale;
+    float s = sin(rot) / scale;
+    return mat3(
+        c, -s, -dot(pos, vec2(c, -s)),
+        s,  c, -dot(pos, vec2(s, c)),
+        0,  0, 1
+    );
+}
 
 void main() {
-    vec2 v = aPos;
 
-    v += entity_pos;
-    v -= cam_pos;
-    v /= zoom;
+    vec3 v = vec3(aPos, 1);
+
+    v *= createMatrix(entity_pos, entity_rot, 1);
+    v *= createMatrixInv(cam_pos, cam_rot, zoom);
 
     v.x *= aspect;
-    gl_Position = vec4(v, 0.0, 1.0);
+    gl_Position = vec4(v.xy, 0.0, 1.0);
 }
